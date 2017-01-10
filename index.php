74beaa9b6e4921f4197e64a594de9bcc1fbcb9c4
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-10 15:52:16 +0100

    Initial commit
    	new file:   default_includes.php
            new file:   index.php
    	new file:   register.php
    	new file:   site.css

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include "default_includes.php" ?>
+<html>
+	<body>
+		<center>
+			<h2>The perfect page to show off your performance in games.</h2>
+			<h5><a href="register.php">Register now.</a></h5>
+		</center>
+	</body>
+</html>
---
+++ register.php
@@ -0,0 +1,15 @@
+<?php include "default_includes.php"; ?>
+<html>
+	<body>
+		<script>
+			function goBack(){
+				window.history.back();
+			}
+		</script>
+		<center>
+			<h1>This page is under construction.</h1>
+			<h3>Sorry about that.</h3>
+			<p><a onClick="goBack()">Return</a></p>
+		</center>
+	</body>
+</html>
