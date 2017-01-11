bc193f7f4abd61a2fee8ef2fecb09319c8300308
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-11 17:23:05 +0100

    Split up register into two files.

--- register.php
+++ register.php
@@ -1,68 +1,23 @@
 <?php
 include "default_includes.php";
-
-$error = "";
-
-if (empty($_POST)){ //if no reg data sent
-	printRegPage();
-} else { //if reg data sent
-	if (isset($_POST["username"] && isset($_POST["password"]) && isset($_POST["email"])){
-		$username = $_POST["username"];
-		$password = $_POST["password"];
-		$email = $_POST["email"];
-		$picture = NULL;
-		$games = "{}";
-		$displayname = "--";
-
-		$password = password_hash($password, PASSWORD_DEFAULT);
-
-		if(isset($_POST["displayname"])) { $displayname = $_POST["displayname"]; }
-		if(isset($_FILES["image"])) { $picture = addslashes(file_get_contents($_FILES['image']['tmp_name'])); }
-
-		if (strlen(trim($email)) == 0 || strlen(trim($username)) == 0 || strlen(trim($password)) == 0){
-			$error = "You didn't enter all required info.";
-			printRegPage();
-			die();
-		}
-
-		$conn = new mysqli($sql_servname, $sql_username, $sql_password, $sql_database);
-
-		if ($conn->connect_error){
-			$error = "Failed to connect to database: " . $conn->connect_error;
-			printRegPage();
-			die();
-		}
-
-		$sql = "INSERT INTO users (username, password, picture, email, games, banned, displayname) VALUES (?, ?, ?, ?, ?, ?, ?)";
-		$stmt = $conn->prepare($sql);
-		$stmt->bind_param("ssbssis", $username, $password, $picture, $email, $games, 0, $displayname);
-		$result = $stmt->execute();
-
-		if ($result === TRUE){
-			printSuccessPage();
-		} else {
-			$error = "Something went wrong: " . $conn->error;
-			printRegPage();
-			die();
-		}
-
-	} else {
-		$error = "You didn't enter all required info.";
-		printRegPage();
-		die();
-	}
-}
-
-function printRegPage(){
-	echo "<html><body>";
-	if ($error != ""){
-		echo "<center><h5 class=\"error\">";
-		echo $error;
-		echo "</h5></center>";
-	}
-	echo "<form action=\"register.php\" method=\"post\"><p>Username*:</p><input type=\"text\" name=\"username\" required><p>Password*:</p><input type=\"password\" name=\"password\" required><p>E-Mail*:</p><input type=\"text\" name=\"email\" required><p>Display name:</p><input type=\"text\" name=\"displayname\"><p>Profile picture:</p><input name=\"image\" id=\"image\" accept=\"image/JPEG\" type=\"file\"><p>* = required</p></body></html>";
-}
-
-function printSuccessPage(){
-	echo "<html><body><h1>Registration successful.</h1></body></html>";
-}
+?>
+<html>
+	<body>
+			<form action="register_backplane.php" method="post">
+				<p>Username*:</p>
+				<input type="text" name="username" required>
+				<p>Password*:</p>
+				<input type="password" name="password" required>
+				<p>E-Mail*:</p>
+				<input type="text" name="email" required>
+				<p>Display name:</p>
+				<input type="text" name="displayname">
+				<p>Profile picture:</p>
+				<input name="image" id="image" accept="image/JPEG" type="file">
+				<br>
+				<br>
+				<input type="submit" name="submit" value="Register">
+				<p>* = required</p>
+			</form>
+	</body>
+</html>
---
+++ register_backplane.php
@@ -0,0 +1,65 @@
+<?php
+include "default_includes.php";
+include "mysql.php";
+
+if (isset($_POST["username"] && isset($_POST["password"]) && isset($_POST["email"])){
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$email = $_POST["email"];
+	$picture = NULL;
+	$games = "{}";
+	$displayname = "--";
+
+	$password = password_hash($password, PASSWORD_DEFAULT);
+
+	if(isset($_POST["displayname"])) { $displayname = $_POST["displayname"]; }
+	if(isset($_FILES["image"])) { $picture = addslashes(file_get_contents($_FILES['image']['tmp_name'])); }
+
+	if (strlen(trim($email)) == 0 || strlen(trim($username)) == 0 || strlen(trim($password)) == 0){
+		printErrorPage("You didn't enter all required info.");
+		die();
+	}
+
+	$conn = new mysqli($sql_servname, $sql_username, $sql_password, $sql_database);
+
+	if ($conn->connect_error){
+		printErrorPage("Failed to connect to database: " . $conn->connect_error);
+		die();
+	}
+
+	$sql = "INSERT INTO users (username, password, picture, email, games, banned, displayname) VALUES (?, ?, ?, ?, ?, ?, ?)";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("ssbssis", $username, $password, $picture, $email, $games, 0, $displayname);
+	$result = $stmt->execute();
+
+	if ($result === TRUE){
+		printSuccessPage();
+	} else {
+		printErrorPage("Something went wrong: " . $conn->error);
+		die();
+	}
+} else {
+	printErrorPage("You didn't enter all required info.");
+	die();
+}
+
+function printErrorPage($error){
+	echo printHtmlPage("<center><h5 class=\"error\">" . $error . "</h5></center>");
+}
+
+function printSuccessPage(){
+	echo printHtmlPage(printCentered("<h5 class=\"success\">Registration successful.</h5>" . printUrl("login.php", "Click here to log in.")));
+}
+
+function printHtmlPage($text){
+	return "<html><body>" . $text . "</body></html>";
+}
+
+function printUrl($url, $text){
+	return "<a href=\"" . $url . "\">" . $text . "</a>";
+}
+
+function printCentered($inner){
+	return "<center>" . $inner . "</center>";
+}
+?>
