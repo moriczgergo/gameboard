6fb11c3833c7da060d5e175424da62c36ddc7be4
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-11 20:45:41 +0100

    Switched logo from h1 to img

--- default_includes.php
+++ default_includes.php
@@ -10,7 +10,7 @@
 		<title>GameBoard</title>
 	</head>
 	<body>
-		<center><h1 class="logo"><a href="index.php" class="logolink">GameBoard</a></h1></center>
+		<center><a href="index.php" class="logolink"><img src="logo.png" alt="GameBoard"></img></a></center>
 		<hr>
 		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
