6f1c03a817bacf8b29fa2c8bcb5945773d9ca977
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-12 17:04:08 +0100

    Fixed logo centering and bg color

--- default_includes.php
+++ default_includes.php
@@ -5,6 +5,11 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 		<link rel="stylesheet" href="site.css">
 		<title>GameBoard</title>
