2861664f44cf8d7cf6b320912e0b58c0934f7fc2
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-17 17:07:50 +0100

    Added kinda dangeorus debugging (please don't steal my Steam API Key)

--- steamtoolkit.php
+++ steamtoolkit.php
@@ -7,6 +7,7 @@ function getSteamLongID($customURL){
 	curl_setopt($ch, CURLOPT_URL, "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?" . http_build_query(array('key' => $steamapikey, 'vaintyurl' => $customURL)));
 	var_dump(http_build_query(array('key' => $steamapikey, 'vaintyurl' => $customURL)));
 	$content = curl_exec($ch);
+	var_dump($steamapikey);
 	var_dump($content);
 	$responseObject = json_decode($content);
 	if ($responseObject === NULL) {
