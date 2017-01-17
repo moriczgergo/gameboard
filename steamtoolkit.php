1c57845c8362a5cfadd00766d1158d4ae2d48e02
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-17 18:01:29 +0100

    Had to convert json objects into arrays but objects in the json object when converting to array havent been converted to array.

--- steamtoolkit.php
+++ steamtoolkit.php
@@ -4,13 +4,13 @@ function getSteamLongID($customURL, $steam){
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_URL, "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" . $steam . "&vanityurl=" . preg_replace('/\s+/', '+', $customURL));
 	$content = curl_exec($ch);
-	var_dump($content);
 	$responseObject = json_decode($content);
 	if ($responseObject === NULL) {
 		return 1; // response wasn't json
 	}
 	$responseArray = (array)$responseObject;
-	$success = $responseArray["response"]["success"];
+	$responseArrayResponseArrray = (array)$responseArray["response"];
+	$success = $responseArrayResponseArrray["success"];
 	if($success == 42){
 		return 2; // user not found
 	} elseif ($success == 1) {
@@ -30,11 +30,12 @@ function getOwnedGames($steamID, $steam){
 		return 1; // response wasn't json
 	}
 	$responseArray = (array)$responseObject;
-	$game_count = $responseArray["response"]["game_count"];
+	$responseArrayResponseArrray = (array)$responseArray["response"];
+	$game_count = $responseArrayResponseArrray["game_count"];
 	if($game_count == 0){
 		return 2; // no games
 	}
-	$games = $responseArray["response"]["games"];
+	$games = (array)$responseArrayResponseArrray["games"];
 	$gameidarr = array();
 	foreach ($games as $gamearr) {
 		array_push($gameidarr, $gamearr["appid"]);
@@ -55,7 +56,8 @@ function getGameName($appid, $steam){
 	if($responseArray == array()){
 		return 2; // game doesn't exist
 	}
-	return $responseArray["game"]["gameName"];
+	$responseArrayGameArray = (array)$responseArray["response"];
+	return $responseArrayGameArray["gameName"];
 }
 
 function getAchievedAchievementsCount($appid, $steamid, $steam){ //NOTE: I needed to use -1 and -2 here, because Steam may send 1 or 2 as response, and error handling would confuse.
@@ -68,11 +70,12 @@ function getAchievedAchievementsCount($appid, $steamid, $steam){ //NOTE: I neede
 		return -1; // response wasn't json
 	}
 	$responseArray = (array)$responseObject;
-	$success = $responseArray["playerstats"]["success"];
+	$responseArrayPlayerstatsArray = (array)$responseArray["playerstats"];
+	$success = $responseArrayPlayerstatsArray["success"];
 	if ($success == false){
 		return -2; //User doesn't have this game.
 	}
-	$achievements = $responseArray["playerstats"]["achievements"];
+	$achievements = $responseArrayPlayerstatsArray["achievements"];
 	$achievedAchievements = 0;
 	foreach ($achievements as $achievement) {
 		if($achievement["achieved"] == 1){
